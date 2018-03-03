01c47e3e756cb76f510e13afa64441cbfa8691ed
Israel Arunah <israel.arunah@konga.com>
2018-03-03 12:16:18 +0100

    modularized getLocation

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -38,14 +38,12 @@ function getPosition() {
 }
 
 $('#getLocation').on('click', function () {
-
+    getPosition();
 });
 
 $('#goGetLocation').on('click', function() {
     let input = $('#locationInput')[0];
-
-
-
+    getPosition();
 });
 
 $('#locationInput').on('input', function () {
@@ -83,46 +81,46 @@ $('#locationInput').on('input', function () {
 
 
 
+//Example from google
 
-
-
-var map;
-var infowindow;
-
-function initMap() {
-    var pyrmont = {lat: -33.867, lng: 151.195};
-
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: pyrmont,
-        zoom: 15
-    });
-
-    infowindow = new google.maps.InfoWindow();
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch({
-        location: pyrmont,
-        radius: 500,
-        type: ['store']
-    }, callback);
-}
-
-function callback(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
-        }
-    }
-}
-
-function createMarker(place) {
-    var placeLoc = place.geometry.location;
-    var marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location
-    });
-
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent(place.name);
-        infowindow.open(map, this);
-    });
-}
+//
+// var map;
+// var infowindow;
+//
+// function initMap() {
+//     var pyrmont = {lat: -33.867, lng: 151.195};
+//
+//     map = new google.maps.Map(document.getElementById('map'), {
+//         center: pyrmont,
+//         zoom: 15
+//     });
+//
+//     infowindow = new google.maps.InfoWindow();
+//     var service = new google.maps.places.PlacesService(map);
+//     service.nearbySearch({
+//         location: pyrmont,
+//         radius: 500,
+//         type: ['store']
+//     }, callback);
+// }
+//
+// function callback(results, status) {
+//     if (status === google.maps.places.PlacesServiceStatus.OK) {
+//         for (var i = 0; i < results.length; i++) {
+//             createMarker(results[i]);
+//         }
+//     }
+// }
+//
+// function createMarker(place) {
+//     var placeLoc = place.geometry.location;
+//     var marker = new google.maps.Marker({
+//         map: map,
+//         position: place.geometry.location
+//     });
+//
+//     google.maps.event.addListener(marker, 'click', function() {
+//         infowindow.setContent(place.name);
+//         infowindow.open(map, this);
+//     });
+// }
